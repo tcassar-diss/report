fc4189bbcd35990029627f4542c0c479662e774a
Tom Cassar <cassar.thomas.e@gmail.com>
2025-05-05 17:27:09 +0100

    Remove screencast script from project word count

--- count.sh
+++ count.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git ls-files '*.tex' | egrep -v '[a-z/]*-old.tex|appendices.tex|title.tex|todos.tex' | xargs texcount | grep -A 8 Total
+git ls-files '*.tex' | egrep -v '[a-z/]*-old.tex|appendices.tex|title.tex|todos.tex|screencast/[a-z]*' | xargs texcount | grep -A 8 Total
